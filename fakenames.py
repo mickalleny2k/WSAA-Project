70628624cc6caec5b3dab382d6bb983ee90d6d8e
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2024-05-16 11:15:10 +0100

    PPSN column
    I added a PPSN column. Of course, they are not real PPS Numbers.
    To create random numbers and letters, I imported the random module.
    And I removed the index column from the csv file. I don't need it in the html file because there's an id column.

--- fakenames.py
+++ fakenames.py
@@ -1,9 +1,21 @@
 from faker import Faker
 import csv
 from random import randrange
+from random import randint 
 import pandas as pd
 import requests
 import json
+import string
+import random
+
+#random_age = round(random.uniform(18.0, 70.0), 1)
+values = randint(0,1000000)
+#print(values)
+ 
+# Randomly choose a letter from all the ascii_letters
+randomLetter = random.choice(string.ascii_letters)
+#print(randomLetter)
+ppsn = (str(values)+randomLetter)
 
 def html():
     fake = Faker()
@@ -13,18 +25,21 @@ def html():
     for i in range(0,10):
         print(i,fake.name())
 
-    #random_age = round(random.uniform(18.0, 70.0), 1)
+    
 
     with open('fakenames.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['id', 'Name','Age'])
-        for n in range(1, 100):
-            writer.writerow((n,fake.name(), randrange(18,70)))
+        writer.writerow(['id', 'Name','Age', 'PPSN'])
+        for n in range(1, 10):
+            values = randint(1000000,9999999)
+            randomLetter = random.choice(string.ascii_letters)
+            ppsn = (str(values)+randomLetter)
+            writer.writerow((n,fake.name(), randrange(18,70),ppsn))
             
-    a = pd.read_csv("fakenames.csv") 
+    a = pd.read_csv("fakenames.csv",index_col=False) 
     # to save as html file 
     # named as "Table" 
-    a.to_html("fakenames.html") 
+    a.to_html("fakenames.html",index=False) 
     # assign it to a  
     # variable (string) 
     html_file = a.to_html()
