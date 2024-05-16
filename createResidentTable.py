0fe1d5c5212e480e8fef6d2b48b98260396db1fb
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2024-05-16 14:35:02 +0100

    Create second table
    Create a second table in the SQL database

---
+++ createResidentTable.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="simonproject"
+)
+
+cursor = db.cursor()
+sql="CREATE TABLE resident (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), age INT, ppsn VARCHAR(250))"
+cursor.execute(sql)
+
+db.close()
+cursor.close()
